772ba54b9cc96da1818c3c64cf5db98449b14f04
Rianhahaha <triandiapriliio@gmail.com>
2026-02-18 15:23:22 +0700

    metadata research, modal metadat, prerender falsea

--- src/routes/meta/+layout.server.ts
+++ src/routes/meta/+layout.server.ts
@@ -1,6 +1,6 @@
 import { PUBLIC_STRAPI_BASE_URL } from '$env/static/public';
 import type { LayoutServerLoad } from './$types';
-
+export const prerender = false;
 export const load: LayoutServerLoad = async ({ fetch }) => {
     const res = await fetch(`${PUBLIC_STRAPI_BASE_URL}/api/articles?populate=*`);
     const data = await res.json();
