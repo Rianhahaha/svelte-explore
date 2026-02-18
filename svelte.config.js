bcc5db85ef18d8a2927e3e585170f963e70887ea
Rianhahaha <triandiapriliio@gmail.com>
2026-02-18 13:07:55 +0700

    metadata research

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-auto'; // for vercel
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto'; // for vercel
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
