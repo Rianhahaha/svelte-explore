e388244e247757f60b911125a987da1416db0643
Rianhahaha <triandiapriliio@gmail.com>
2026-02-18 13:08:29 +0700

    metadata research

--- svelte.config.js
+++ svelte.config.js
@@ -8,32 +8,32 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined, // Wajib undefined kalau mau murni static
-            precompress: false,
-            strict: true
-        }),
-        prerender: {
-            // SvelteKit kadang bingung kalau ada link yang error (misal 404).
-            // 'warn' biar build gak gagal total cuma gara-gara 1 gambar hilang.
-            handleHttpError: 'warn', 
+// kit: {
+//         adapter: adapter({
+//             pages: 'build',
+//             assets: 'build',
+//             fallback: undefined, // Wajib undefined kalau mau murni static
+//             precompress: false,
+//             strict: true
+//         }),
+//         prerender: {
+//             // SvelteKit kadang bingung kalau ada link yang error (misal 404).
+//             // 'warn' biar build gak gagal total cuma gara-gara 1 gambar hilang.
+//             handleHttpError: 'warn', 
             
-            // concurrency: 1 // Nyalakan ini kalau Strapi servermu kentang banget biar gak jebol
-        }
-    }
+//             // concurrency: 1 // Nyalakan ini kalau Strapi servermu kentang banget biar gak jebol
+//         }
+//     }
 
 	
 	// preprocess: vitePreprocess(),
 
-	// kit: {
-	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	// 	adapter: adapter()
-	// }
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
