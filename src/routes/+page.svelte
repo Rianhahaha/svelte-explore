d918392dcbb05cb5749bdada0c49484fbce85b7f
Rianhahaha <triandiapriliio@gmail.com>
2026-02-20 11:17:21 +0700

    added staging branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 </svelte:head>
 <section class="bg-amber-500 w-full h-dvh flex items-center justify-center">
     <div class="flex flex-col justify-center text-center gap-4">
-
+        This is the Staging Branch~
         <a class="py-2 px-5 bg-slate-800 text-white rounded-2xl hover:bg-slate-600" href="/promordo">To Pomodoro</a>
         <a class="py-2 px-5 bg-slate-800 text-white rounded-2xl hover:bg-slate-600" href="/landing">To Simple landing Page</a>
         <a class="py-2 px-5 bg-slate-800 text-white rounded-2xl hover:bg-slate-600" href="/meta">Metadata Research</a>
